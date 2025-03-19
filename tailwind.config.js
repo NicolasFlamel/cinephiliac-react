b19bf45920d9b4e98a9ae44c9e90e6f3a84f3d87
Amauri <hamauri@aol.com>
2025-03-18 17:27:45 -0700

    removed packages
    due to bugs had to remove package-lock and reinstall
    
    updated tailwind config
    
    removed vite plugin checker

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
   content: [
     './index.html',
     './src/**/*.{html,ts,tsx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/cinephiliac-react/',
-  plugins: [
-    react(),
-    viteTsconfigPaths(),
-    checker({
-      typescript: {
-        tsconfigPath: 'tsconfig.app.json',
-      },
-      eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
-      overlay: true,
-    }),
-  ],
+  plugins: [react(), viteTsconfigPaths()],
   server: {
     port: 3000,
   },
