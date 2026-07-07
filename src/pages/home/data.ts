81f0411c54c3c8b8c80f1e2ba48be4867c33041c
Amauri <hamauri@aol.com>
2026-07-07 12:07:50 -0700

    fix(renaming): renamed types

--- src/pages/home/data.ts
+++ src/pages/home/data.ts
@@ -1,14 +1,14 @@
 import type { GameGenreType, GameModeType } from '@/types';
 
-type gameModesList = Array<{ label: string; value: GameModeType }>;
-type genresList = Array<{ label: string; value: GameGenreType }>;
+type GameModesListType = Array<{ label: string; value: GameModeType }>;
+type GenresListType = Array<{ label: string; value: GameGenreType }>;
 
-export const gameModes: gameModesList = [
+export const gameModes: GameModesListType = [
   { label: 'Box Office Mode', value: 'Box-Office' },
   { label: 'Ratings Mode', value: 'Ratings' },
 ];
 
-export const genres: genresList = [
+export const genres: GenresListType = [
   { label: 'All Genres', value: 'All-Genres' },
   { label: 'Action', value: 'Action' },
   { label: 'Animation', value: 'Animation' },
